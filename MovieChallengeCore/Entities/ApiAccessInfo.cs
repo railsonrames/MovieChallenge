9a7732f01562b76fca017eb2298607e044e61354
Railson Rames <rames.dev@outlook.com>
2020-08-27 17:08:00 -0300

    Separando e consultando dados no appsettings.

---
+++ MovieChallengeCore/Entities/ApiAccessInfo.cs
@@ -0,0 +1,8 @@
+﻿namespace MovieChallengeCore.Entities
+{
+    public class ApiAccessInfo
+    {
+        public string Uri { get; set; }
+        public string Key { get; set; }
+    }
+}
--- MovieChallengeCore/Services/MovieService.cs
+++ MovieChallengeCore/Services/MovieService.cs
@@ -1,9 +1,11 @@
-﻿using MovieChallengeCore.Entities;
+﻿using Microsoft.Extensions.Configuration;
+using MovieChallengeCore.Entities;
 using MovieChallengeCore.Interfaces;
 using Newtonsoft.Json.Linq;
 using Refit;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,13 +15,15 @@ namespace MovieChallengeCore.Services
     {
         public async Task<List<Movie>> GetMovies()
         {
+            var accessInfo = GetAccessInfo();
+
             try
             {
-                var client = RestService.For<IMovieApiService>("https://api.themoviedb.org/3");
-                var genreResult = await client.GetGenres("6ccc4110e732d20c6731baad9e5db5df");
+                var client = RestService.For<IMovieApiService>(accessInfo.Uri);
+                var genreResult = await client.GetGenres(accessInfo.Key);
                 var jObjectGenres = JObject.Parse(genreResult);
                 var genreList = jObjectGenres["genres"].ToObject<Genre[]>();
-                var movieResult = await client.GetMovies("6ccc4110e732d20c6731baad9e5db5df");
+                var movieResult = await client.GetMovies(accessInfo.Key);
                 var jObjectMovies = JObject.Parse(movieResult);
                 var movies = jObjectMovies["results"].ToObject<Movie[]>();
                 foreach (var movie in movies)
@@ -36,6 +40,39 @@ namespace MovieChallengeCore.Services
             catch (Exception e)
             {
                 throw e;
+            }
+        }
+
+        //protected async Task<List<Genre>> GetGenreList(IMovieApiService client)
+        //{
+        //    var genreResult = await client.GetGenres("6ccc4110e732d20c6731baad9e5db5df");
+        //    var jObjectGenres = JObject.Parse(genreResult);
+        //    var genreList = jObjectGenres["genres"].ToObject<Genre[]>();
+        //}
+
+        //protected async Task<List<Movie>> GetMovieList()
+        //{
+
+        //}
+
+        protected ApiAccessInfo GetAccessInfo()
+        {
+            var configuration = new ConfigurationBuilder();
+            var file = Path.Combine(Directory.GetCurrentDirectory(), "appsettings.json");
+            configuration.AddJsonFile(file, optional: false);
+            var config = configuration.Build();
+
+            try
+            {
+                return new ApiAccessInfo
+                {
+                    Uri = config.GetSection("ApiUri").Value,
+                    Key = config.GetSection("ApiKey").Value
+                };
+            }
+            catch (Exception exception)
+            {
+                throw exception;
             }
         }
     }
